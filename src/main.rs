c4b1d69c30062cf4fdaf2c6b10a713b6970106a0
지영현/BE/GN <z308520@kia-partners.com>
2025-07-14 16:15:41 +0900

    init: axum

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
