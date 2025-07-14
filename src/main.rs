59343e34b5eb8ac022792666342c103b869b5542
지영현/BE/GN <z308520@kia-partners.com>
2025-07-14 16:15:12 +0900

    init: axum

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use crate::setup_logger::setup_logger;
 pub mod build_info;
 pub mod setup_logger;
 
+use axum::{Router, routing::get};
+
 use mimalloc::MiMalloc;
 
 #[global_allocator]
@@ -11,4 +13,14 @@ static GLOBAL: MiMalloc = MiMalloc;
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let (_log_guard, _stdout_guard) = setup_logger().await;
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
